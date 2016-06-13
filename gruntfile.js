64dfb23c85f6138b1f48b2687fa495679c64249b
DmitrySolo <solo-webworks@mail.ru>
2016-06-13 14:52:58 +0300

    Added Grunt Watch and Live Reload

--- gruntfile.js
+++ gruntfile.js
@@ -10,7 +10,21 @@ module.exports = function(grunt) {
                     "Build/jade_templates/index.html": "Build/jade_templates/index.jade"
                 }
             }
+        },
+        watch: {
+            jade: {
+                // We watch and compile sass files as normal but don't live reload here
+                files: ['Build/jade_templates/index.jade'],
+                tasks: ['jade'],
+            },
+            livereload: {
+                // Here we watch the files the sass task will compile to
+                // These files are sent to the live reload server after sass compiles to them
+                options: { livereload: true },
+                files: ['Build/jade_templates/index.html'],
+            },
         }
 });
     grunt.loadNpmTasks('grunt-contrib-jade');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 };
