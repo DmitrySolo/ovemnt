71b2afd6d2736fa53f8dfa03c900024b65da6870
DmitrySolo <solo-webworks@mail.ru>
2016-06-18 15:19:38 +0300

    jade includes added

--- gruntfile.js
+++ gruntfile.js
@@ -1,3 +1,8 @@
+var jadeFile = "Build/jade_templates/layout.jade";
+var targetHtml = "Production/MainPage/index.html";
+var targetCSS = "Production/MainPage/main.css";
+var scssFiles = "Build/SASS/*.scss";
+
 module.exports = function(grunt) {
     // Do grunt-related things in here
     grunt.initConfig({
@@ -7,24 +12,40 @@ module.exports = function(grunt) {
             },
             compile: {
                 files: {
-                    "Build/jade_templates/index.html": "Build/jade_templates/index.jade"
+                    "Production/MainPage/index.html" : jadeFile
+                }
+            }
+        },
+        sass: {                              // Task
+            dist: {                            // Target
+                options: {                       // Target options
+                    style: 'expanded'
+                },
+                files: {                         // Dictionary of files
+                    'Production/MainPage/main.css': 'Build/SASS/main.scss'
                 }
             }
         },
         watch: {
             jade: {
                 // We watch and compile sass files as normal but don't live reload here
-                files: ['Build/jade_templates/index.jade'],
+                files: [jadeFile],
                 tasks: ['jade'],
+            },
+            sass: {
+                files: [scssFiles],
+                tasks: ['sass']
             },
             livereload: {
                 // Here we watch the files the sass task will compile to
                 // These files are sent to the live reload server after sass compiles to them
                 options: { livereload: true },
-                files: ['Build/jade_templates/index.html'],
+                files: [targetHtml, targetCSS]
             },
         }
 });
+    grunt.loadNpmTasks('grunt-contrib-sass');
     grunt.loadNpmTasks('grunt-contrib-jade');
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.registerTask('default', ['watch']);
 };
